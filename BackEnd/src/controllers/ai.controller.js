2905cdaef2d184aa45a1a55ecabbb7b3ee3cb684
Sourish-07012003 <sourishpanja07@gmail.com>
2025-06-13 21:00:49 +0530

    Fix res.return to res.status

--- BackEnd/src/controllers/ai.controller.js
+++ BackEnd/src/controllers/ai.controller.js
@@ -1,19 +1,39 @@
-const aiService = require("../services/ai.service")
+// const aiService = require("../services/ai.service")
 
 
 
-module.exports.getReview = async (req,res)=>{
+// module.exports.getReview = async (req,res)=>{
 
+//     const code = req.body.code;
+
+//     if(!code)
+//     {
+//         return res.return(400).send("Code is required");
+//     }
+
+//     const response = await aiService(code);
+
+
+//     res.send(response);
+
+// }
+
+
+
+const aiService = require("../services/ai.service");
+
+module.exports.getReview = async (req, res) => {
     const code = req.body.code;
 
-    if(!code)
-    {
-        return res.return(400).send("Code is required");
+    if (!code) {
+        return res.status(400).send("Code is required");  // ✅ Fixed here
     }
 
-    const response = await aiService(code);
-
-
-    res.send(response);
-
-}
+    try {
+        const response = await aiService(code);
+        res.send(response);
+    } catch (error) {
+        console.error("Error in getReview:", error);
+        res.status(500).send("Something went wrong while processing your code.");
+    }
+};
